9cc30ff15b5e09a95f62ea8a7b89c1bf4eb25afe
Duck Developer <91049342+Nutmito@users.noreply.github.com>
2024-01-20 18:42:26 +0700

    Reset

--- Snake/Snake/Form1.Designer.cs
+++ Snake/Snake/Form1.Designer.cs
@@ -28,10 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            SuspendLayout();
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            MaximizeBox = false;
+            Name = "Form1";
+            Text = "Snake Game";
+            ResumeLayout(false);
         }
 
         #endregion
